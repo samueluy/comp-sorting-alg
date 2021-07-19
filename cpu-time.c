e8ca7d157efc667aba7bbe998dbb9794e3e5b9e6
Samuel Uy <74288590+samueluy@users.noreply.github.com>
2021-07-19 14:23:43 +0800

    Added insertion and selection

--- cpu-time.c
+++ cpu-time.c
@@ -5,18 +5,11 @@
 
 // Files
 #include "bubble.c"
+#include "insertion.c"
+#include "selection.c"
 #include "GenerateArray.c"
 
-
-int main () {
-	
-	srand(time(NULL)); // set random seed for randomization
-	
-	int N[100000];
-    int i, y, nSize;
-    double time;
-
-int measureMET(int array[], int size){
+int bubbleMeasureMET(int array[], int size){
     // Start measuring time
     struct timespec begin, end; 
 	
@@ -32,7 +25,7 @@ int measureMET(int array[], int size){
 	#endif    
 	
 	    /* Call sorting function */
-		bubble_sort(N, nSize);
+		bubble_sort(array, size);
 	    
 	    // Stop measuring time and calculate the elapsed time
 	#if CPU_TIME 
@@ -55,6 +48,60 @@ int measureMET(int array[], int size){
 	    return elapsed*1000;
 	}
     
+int insertionMeasureMET(int array[], int size){
+    struct timespec begin, end; 
+	
+	#if CPU_TIME
+	    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+	
+	#else
+	    clock_gettime(CLOCK_REALTIME, &begin);
+	
+	#endif    
+		insertion_sort(array, size);
+
+	#if CPU_TIME 
+	    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);   
+	
+	#else
+	   clock_gettime(CLOCK_REALTIME, &end);
+	   
+	#endif
+		long seconds = end.tv_sec - begin.tv_sec;
+	    long nanoseconds = end.tv_nsec - begin.tv_nsec;
+		double elapsed = seconds + nanoseconds*1e-9;
+		
+	    printf("CPU Time measured: %lf in miliseconds.\n", elapsed*1000);    
+	    return elapsed*1000;
+}
+
+int selectionMeasureMET(int array[], int size){
+    struct timespec begin, end; 
+	
+	#if CPU_TIME
+	    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+	
+	#else
+	    clock_gettime(CLOCK_REALTIME, &begin);
+	
+	#endif    
+		selection_sort(array, size);
+
+	#if CPU_TIME 
+	    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);   
+	
+	#else
+	   clock_gettime(CLOCK_REALTIME, &end);
+	   
+	#endif
+		long seconds = end.tv_sec - begin.tv_sec;
+	    long nanoseconds = end.tv_nsec - begin.tv_nsec;
+		double elapsed = seconds + nanoseconds*1e-9;
+		
+	    printf("CPU Time measured: %lf in miliseconds.\n", elapsed*1000);    
+	    return elapsed*1000;
+}
+	
 int exponent(int base, int power){ // get value when raised to a power
 	int result=1;
     	while(power != 0){
@@ -64,15 +111,48 @@ int exponent(int base, int power){ // get value when raised to a power
 	return result;
 }
 
+void arrayCopy(int source[], int target[], int size){
+	int i;
+	for(i=0; i<size; i++){
+		target[i] = source[i];
+	}
+}
+
+int main () {
+	srand(time(NULL)); // set random seed for randomization
+	
+	int N1[100000], N2[100000], N3[100000];
+    int i, y, nSize;
+    double bubble_time, insertion_time, selection_time;
+
     for(i=0; i<6; i++){ // 6 different sizes of arrays
-    	nSize = exponent(2, i+10); // set size of array
-    	printf("Current array size: %d\n", nSize);
+    	nSize = exponent(2, i+12); // set size of array
+    	printf("Current array size: %d\n\n", nSize);
     	for(y=0; y<10; y++){
-    		GenerateData(N, nSize);
-    		time += measureMET(N, nSize);
+    		printf("Test#%d/10\n", y+1);
+    		
+    		// Generate arrays
+    		GenerateData(N1, nSize); // Generate base array
+    		arrayCopy(N1, N2, nSize);
+    		arrayCopy(N1, N3, nSize);
+    		
+    		// Print results
+    		printf("Bubble: ");
+    		bubble_time += bubbleMeasureMET(N1, nSize);
+    		printf("Insertion: ");
+    		insertion_time += insertionMeasureMET(N2, nSize);
+    		printf("Selection: ");
+    		selection_time += selectionMeasureMET(N3, nSize);
+    		printf("\n");
 		}
-		printf("Average time: %lf miliseconds", time/10);
-		time=0;
+		printf("----------------------------------------------------------\n");
+		printf("Bubble = Average time: %lf miliseconds\n", bubble_time/10);
+		printf("Insertion = Average time: %lf miliseconds\n", insertion_time/10);
+		printf("Selection = Average time: %lf miliseconds\n", selection_time/10);
+		printf("----------------------------------------------------------\n");
+		bubble_time=0;
+		insertion_time=0;
+		selection_time=0;
     	printf("\nEND\n\n");
 	}
     
